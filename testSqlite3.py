8f1dd6d3818cee16a299438a89a4df579b4120d2
Tomohiro Shimizu <cybergun00@gmail.com>
2019-09-25 15:52:24 +0900

    a few changes

--- testSqlite3.py
+++ testSqlite3.py
@@ -3,11 +3,17 @@ import sqlite3
 connector = sqlite3.connect('test.sqlite3')
 cur = connector.cursor()
 
+users_data = [(2, "Kouji Tadokoro", "Senior", 19.19),
+              (3, "Naoki Kimura", "Debut", 8.1),
+              (4, "Daichi Miura", "Grand Senior", 114.514)]
+
 # 以下SQL
+# テーブル作成
 # cur.execute('create table users(id integer, name text, title text, score real)')
 
 # 書き込み
-# cur.execute('insert into users values(1, "HentaiKusoDokata", "Unchi", 19.19)')
+# cur.execute('insert into users values(1, "Taro Tanaka", "Debut", 0.0)')
+# cur.executemany('insert into users values(?, ?, ?, ?)', users_data)
 
 # 読み込み
 # cur.execute('select * from users')
